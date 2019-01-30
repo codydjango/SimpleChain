96fa4bf46c906eeaf7f2ca7e0043060e8107408e
Cody Django Redmond <codydjango@gmail.com>
2019-01-30 13:55:59 -0800

    add getBlockByHeight controller method

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const BlockChain = require('./BlockChain.js')
+const Block = require('./Block.js')
+
 const app = express()
 const port = 8000
 const router = express.Router()
-const bodyParser = require('body-parser')
+const myBlockChain = new BlockChain()
 
-router.get('/', (req, res) => {
-    res.json('Hello World!')
-})
+router.get('/block/:height', getBlockByHeight)
 
-app.use(bodyParser.json())
-app.use(router)
-app.listen(port, listening)
+
+;(async () => {
+    await myBlockChain.onReady()
+
+    app.use(bodyParser.json())
+    app.use(router)
+    app.listen(port, listening)
+})()
+
+
+async function getBlockByHeight(req, res) {
+    const block = await myBlockChain.getBlock(req.params.height)
+
+    res.json(block)
+}
+
 
 function listening() {
-	console.log(`API listening on localhost:${port}`)
+    console.log(`API listening on localhost:${port}`)
 }
